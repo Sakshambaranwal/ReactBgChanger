17b23ea0d50a9d7a8f63c230b6736117d1ff5b79
Saksham Kumar <sakshamkumar101099@gmail.com>
2025-04-21 01:04:52 +0530

    color input

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   
 
   return (
-    <div className="!h-[100vh] !w-[100vw]" style={{backgroundColor:`rgb(${red},${blue},${green})`}}>
+    <div className="!h-[100vh] !w-[100vw]" style={{backgroundColor:`rgb(${red},${green},${blue})`}}>
       <div className='p-3 rounded-full fixed bottom-6 left-[50%] transform -translate-x-1/2 bg-gray-200 flex gap-[4px] w-fit'>
         <input className='bg-red-500 p-2 flex-1 text-white rounded-full text-center' value={red} onChange={(e)=>setRed(e.target.value)}></input>
         <input className='bg-blue-500 p-2 flex-1 text-white rounded-full text-center' value={blue}onChange={(e)=>setBlue(e.target.value)}></input>
