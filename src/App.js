2c5a90885d3a5b304e25ade38248ebae5b9101c0
Saksham Kumar <sakshamkumar101099@gmail.com>
2025-04-21 00:44:11 +0530

    bg color chnager react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [red, setRed] = useState(255)
+  const [blue, setBlue] = useState(255)
+  const [green, setGreen] = useState(255)
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="!h-[100vh] !w-[100vw]" style={{backgroundColor:`rgb(${red},${blue},${green})`}}>
+      <div className='p-3 rounded-full fixed bottom-6 left-[50%] transform -translate-x-1/2 bg-gray-200 flex gap-[4px] w-fit'>
+        <input className='bg-red-500 p-2 flex-1 text-white rounded-full text-center' value={red} onChange={(e)=>setRed(e.target.value)}></input>
+        <input className='bg-blue-500 p-2 flex-1 text-white rounded-full text-center' value={blue}onChange={(e)=>setBlue(e.target.value)}></input>
+        <input className='bg-green-500 p-2 flex-1 text-white rounded-full text-center' value={green} onChange={(e)=>setGreen(e.target.value)}></input>
+      </div>
     </div>
   );
 }
